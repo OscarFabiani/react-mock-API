e3daf2ac2680456a4e0ef0a8e27584532253423e
OscarFabiani <spartan539@yahoo.com>
2019-03-23 22:37:38 -0700

    Continued refinig components.

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const PRODUCTS = [
   {category: "Sporting Goods", price: "$49.99", stocked: true, name: "Football"},
   {category: "Sporting Goods", price: "$9.99", stocked: true, name: "Baseball"},
   {category: "Sporting Goods", price: "$29.99", stocked: false, name: "Basketball"},
-  {category: "Electronics", price: "$99.99", stocked: true, name: "iPod Touch"},
+  {category: "Electronics", price: "$99.99", stocked: false, name: "iPod Touch"},
   {category: "Electronics", price: "$399.99", stocked: false, name: "iPhone 5"},
   {category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7"}
 ];
@@ -40,16 +40,48 @@ class ProductRow extends React.Component {
   }
 }
 
+
 class ProductTable extends React.Component {
   render() {
+    const filterText = this.props.filterText;
+    const inStockOnly = this.props.inStockOnly;
     let rows = [];
     let lastCategory = null;
-    this.props.products.forEach(product => {
-      (product.category !== lastCategory) &&
-        (rows = [...rows, <ProductCategoryRow category={product.category} key={product.category}/>]);
-      rows = [...rows, <ProductRow product={product} key={product.name}/>];
-      lastCategory = product.category;
+    this.props.products.forEach(prod => {
+      if (prod.category !== lastCategory) {
+        rows = [...rows, <ProductCategoryRow category={prod.category} key={prod.category}/>];
+      }
+      rows = [...rows, <ProductRow product={prod} key={prod.name}/>];
+      lastCategory = prod.category;
     });
+    //example code
+    this.props.products.forEach((prod) => {
+      if (prod.name.indexOf(filterText) === -1) {
+        return;
+      }
+      if (inStockOnly && !prod.stocked) {
+        return;
+      }
+      if (prod.category !== lastCategory) {
+        rows.push(
+          <ProductCategoryRow
+            category={prod.category}
+            key={prod.category} />
+        );
+      }
+      rows = [...rows, <ProductRow product={prod} key={prod.name}/>];
+      lastCategory = prod.category;
+    });
+    //me trying to refine logic using ternary TRY TO MAKE THIS WORK
+    this.props.products.filter((prod) =>
+       (prod.name.indexOf(filterText) !== -1) ? 
+        (!inStockOnly || prod.stocked) ?
+          (prod.category !== lastCategory) ? true : false
+        : false : false).forEach(prod => {
+          rows.push(<ProductCategoryRow category={prod.category} key={prod.category} />);
+          rows = [...rows, <ProductRow product={prod} key={prod.name}/>];
+          lastCategory = prod.category;
+      })
     return (
       <table className='ProductTable'>
         <thead>
@@ -64,13 +96,16 @@ class ProductTable extends React.Component {
   }
 }
 
+
 class SearchBar extends React.Component {
   render() {
+    const filterText = this.props.filterText;
+    const inStockOnly = this.props.inStockOnly;
     return (
       <form className='SearchBar'>
-        <input type='text' placeholder='Search...'/>
+        <input type='text' placeholder='Search...' value={filterText}/>
         <div>
-          <input type='checkbox'/>
+          <input type='checkbox' checked={inStockOnly}/>
           <span>Only show products in stock</span>
         </div>
       </form>
@@ -79,11 +114,18 @@ class SearchBar extends React.Component {
 }
 
 class FilterableProductTable extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filterText: '',
+      inStockOnly: false,
+    }
+  }
   render() {
     return (
       <div className='FilterableProductTable'>
-        <SearchBar/>
-        <ProductTable products={this.props.products}/>
+        <SearchBar filterText={this.state.filterText} inStockOnly={this.state.inStockOnly}/>
+        <ProductTable products={this.props.products} filterText={this.state.filterText} inStockOnly={this.state.inStockOnly}/>
       </div>
     );
   }
@@ -94,6 +136,61 @@ class FilterableProductTable extends React.Component {
 ReactDOM.render(<FilterableProductTable products={PRODUCTS} />, document.getElementById('root'));
 
 
+/*
+This is an alternate which combines the ProductCategoryRow, ProductRow, and ProductTable components. Besides condensing
+components, which makes the components' JavaScript before the render look more like a Vanilla.JS project, this component
+assumes which categories are available, wheras the example component instead assumes the objects will be sorted by
+category.
+NOTE: I should test this component while  introducing state to see the difference between having seperate components
+instead of combining them.
+
+class ProductTable extends React.Component {
+  render() {
+    let sportingGoods = this.props.products.filter(p => p.category === 'Sporting Goods');
+    let electronics = this.props.products.filter(p => p.category === 'Electronics');
+    const sportingGoodsRows = sportingGoods.map(p => {
+      let name = p.stocked ? p.name : <span style={{color: 'red'}}>{p.name}</span>;
+      return (
+      <tr className='ProductRow' key={name}>
+        <td>{name}</td>
+        <td>{p.price}</td>
+      </tr>
+      )
+    })
+    const electronicsRows = electronics.map(p => {
+      let name = p.stocked ? p.name : <span style={{color: 'red'}}>{p.name}</span>;
+      return (
+      <tr className='ProductRow' key={name}>
+        <td>{name}</td>
+        <td>{p.price}</td>
+      </tr>
+      )
+    })
+    return (
+      <table className='ProductTable'>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+        <tr className='ProductCategoryRow'>
+          <th colSpan='2'>Sporting Goods</th>
+        </tr>
+        {sportingGoodsRows}
+        <tr className='ProductCategoryRow'>
+          <th colSpan='2'>Electronics</th>
+        </tr>
+        {electronicsRows}
+      </tbody>
+      </table>
+    )
+  }
+}
+*/
 
 
-//CONTINUE  DISECTING CODE AND CONTINUE TUTORIAL FROM 'STEP 3: IDENTIFY THE MINIMAL...'
+
+
+//CONTINUE DISSECTING CODE AND CONTINUE TUTORIAL FROM 'STEP 4: IDENTIFY THE MINIMAL...'
